b2d11467661eacbcf4c2a157f8acb2bedf54cdec
Iakov Polyak <iakov.polyak@linaro.org>
2026-01-16 16:10:23 +0000

    Little test edit.

--- tests/system_health.rs
+++ tests/system_health.rs
@@ -4,12 +4,6 @@ use tux_validation::os_release;
 fn debian_and_forky() {
     let osr = os_release::parse_os_release("/etc/os-release").expect("Failed to read os-release");
 
-    //println!("ID={}", osr.get("ID").map(String::as_str).unwrap_or("<missing>"));
-    //println!(
-    //    "VERSION_CODENAME={}",
-    //    osr.get("VERSION_CODENAME").map(String::as_str).unwrap_or("<missing>")
-    //);
-
     assert_eq!(osr.get("ID").map(String::as_str), Some("debian"), "Not running Debian!");
-    assert_eq!(osr.get("VERSION_CODENAME").map(String::as_str), Some("forky"), "Wrong image version!");
+    assert_eq!(osr.get("VERSION_CODENAME").map(String::as_str), Some("forky"), "Codename is not `forky`!");
 }
