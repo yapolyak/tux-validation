4941d86b4a2241304f9eb91d941f584689a28ed1
Iakov Polyak <iakov.polyak@linaro.org>
2026-02-19 14:53:18 +0000

    Implemented a general wrapper over udev::Enumerator.scan_devices for a specific subsystem.

---
+++ examples/udev_subsystem_info.rs
@@ -0,0 +1,14 @@
+use clap::Parser;
+use tux_validation::utils::subsystem_info_udev;
+
+#[derive(Parser)]
+#[command(author, version, about = "Performs a provided subsystem scan with udev.")]
+struct Args {
+    //Subsystem to perform scan on
+    subsystem: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    subsystem_info_udev(args.subsystem)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod device;
 pub mod i2c;
 pub mod os_release;
+pub mod utils;
